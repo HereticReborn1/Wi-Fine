d5856667809235cbcb09d78362629d5b7c721056
HereticReborn1 <gupta.rohan.257@gmail.com>
2018-04-10 12:37:52 +0530

    Finish basic structure, though some error exist

--- app/src/main/java/com/example/heretic/wi_fine/ListAdapter.java
+++ app/src/main/java/com/example/heretic/wi_fine/ListAdapter.java
@@ -1,6 +1,82 @@
 package com.example.heretic.wi_fine;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.net.wifi.ScanResult;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class ListAdapter extends BaseAdapter {
+
+    private Context context;
+    private LayoutInflater inflater;
+    private List wifiList;
+
+    ListAdapter(Context context1, List wifiList1)
+    {
+        this.context =context1;
+        this.wifiList=wifiList1;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+
+    @Override
+    public int getCount() {
+        return wifiList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Holder holder;
+        System.out.println("viewpos" + position);
+        View view = convertView;
+        if (view == null) {
+            view = inflater.inflate(R.layout.dataset, null);
+            holder = new Holder();
+            holder.tvDetails = view.findViewById(R.id.tvDetails);
+            view.setTag(holder);
+        }
+        else
+        {
+            holder = (Holder) view.getTag();
+        }
+        ScanResult x = (ScanResult) wifiList.get(position);
+        int level = WifiManager.calculateSignalLevel(x.level,4);
+        String s = x.SSID + "\n" + "Strength: " + getStrength(level) + "\nBSSID: " + x.BSSID;
+
+        holder.tvDetails.setText(s);
+        return view;
+    }
+
+    private String getStrength(int level)
+    {
+        switch (level)
+        {
+            case 0: return "Dead";
+            case 1: return "Weak";
+            case 2: return "Fair";
+            case 3: return "Strong";
+            case 4: return "Excellent";
+        }
+        return "";
+    }
+
+    class Holder {
+        TextView tvDetails;
+    }
 }
--- app/src/main/java/com/example/heretic/wi_fine/MainActivity.java
+++ app/src/main/java/com/example/heretic/wi_fine/MainActivity.java
@@ -1,46 +1,107 @@
 package com.example.heretic.wi_fine;
 
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import static android.widget.Toast.makeText;
 
 public class MainActivity extends Activity {
-    Button enableButton,disableButton,scanButton;
+
+    public static List<ScanResult> wifiList;
+    private WifiManager wifi;
+
+
+    Button enableButton, disableButton, scanButton;
+    ListView logList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logList = findViewById(R.id.listview);
+        enableButton= findViewById(R.id.button);
+        disableButton= findViewById(R.id.button2);
+        scanButton = findViewById(R.id.button3);
 
-        enableButton=(Button)findViewById(R.id.button);
-        disableButton=(Button)findViewById(R.id.button2);
-        scanButton = (Button)findViewById(R.id.button3);
+        wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiReceiver receiver = new WifiReceiver();
+        registerReceiver(receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
 
         enableButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                assert wifi != null;
                 wifi.setWifiEnabled(true);
+                Toast.makeText(getApplicationContext(),"Wifi Enabled",Toast.LENGTH_SHORT).show();
             }
         });
 
         disableButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                assert wifi != null;
                 wifi.setWifiEnabled(false);
+                Toast.makeText(getApplicationContext(),"Wifi Disabled",Toast.LENGTH_SHORT).show();
             }
         });
 
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),Scan.class);
-                startActivity(i);
-                setContentView(R.layout.scan);
-
+                startScan();
             }
         });
+
+
     }
+
+    public void setAdapter()
+    {
+        ListAdapter adapter = new ListAdapter(getApplicationContext(), wifiList);
+        logList.setAdapter(adapter);
+    }
+
+    @SuppressLint("ShowToast")
+    protected void startScan() {
+        //strengthLog.add("Time\t\tStrength Level");
+
+
+                int k=0;
+                while (k<12) {
+                    wifi.startScan();
+                    wifiList = wifi.getScanResults();
+                    setAdapter();
+                    try {
+                        Thread.sleep(5000);
+                    }
+                    catch (Exception ignored)
+                    {
+
+                    }
+                    makeText(getApplicationContext(), "Scan finished", Toast.LENGTH_SHORT).show();
+                    k++;
+                }
+
+
+
+    }
+
+
+    class WifiReceiver extends BroadcastReceiver {
+        public void onReceive(Context c, Intent intent) {
+        }
+    }
+
 }
